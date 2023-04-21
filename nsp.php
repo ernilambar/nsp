c2dde5c1b0536b6c2ac91edf4a93a36c33e2ea8b
Nilambar Sharma <nilambar@outlook.com>
2023-04-21 21:39:17 +0545

    fix: update version to 1.0.2

--- nsp.php
+++ nsp.php
@@ -3,7 +3,7 @@
  * Plugin Name: NSP
  * Plugin URI: https://github.com/ernilambar/nsp
  * Description: Dev helpers.
- * Version: 1.0.1
+ * Version: 1.0.2
  * Author: Nilambar Sharma
  * Author URI: https://www.nilambar.net/
  * Text Domain: nsp
@@ -18,7 +18,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'NSP_VERSION', '1.0.1' );
+define( 'NSP_VERSION', '1.0.2' );
 define( 'NSP_SLUG', 'nsp' );
 define( 'NSP_BASE_NAME', basename( __DIR__ ) );
 define( 'NSP_BASE_FILEPATH', __FILE__ );
